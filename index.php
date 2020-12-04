ae19da154da9690ab0c002a0715d3228e22db92d
santy_naren <santhoshnarendra@gmail.com>
2020-12-04 17:32:33 +0530

    init

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$name = $_POST['firstName'];
+$emailId = $_POST['emailId'];
+$number = $_POST['phoneNumber'];
+$message= $_POST['message'];
+
+echo '<b>'.$name.'</b> '.$emailId.'<br>';
+echo $emailId.'<br>';
+echo $number.'<br>';
+echo $message.'<br>';
+
+
+
+
+?>
