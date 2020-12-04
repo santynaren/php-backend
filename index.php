5a305e17b14fd0316ddbfcd3481099e1b73d0266
santy_naren <santhoshnarendra@gmail.com>
2020-12-04 18:24:21 +0530

    init

--- index.php
+++ index.php
@@ -4,6 +4,23 @@ $emailId = $_POST['emailId'];
 $number = $_POST['phoneNumber'];
 $message= $_POST['message'];
 
+$username = "root";
+$password="";
+$servername = "localhost";
+$database = "portfoliodb";
+
+$connection = new mysqli($servername,$username,$password,$database);
+if($connection->connect_errno){
+    echo"Failed";
+}else{
+    $insert = "INSERT INTO contactForm  VALUES('','$name','$emailId','$number','$message')";
+if($connection->query($insert)===TRUE){
+    echo "dATA SENT";
+}else{
+echo $connection->error;
+}
+}
+
 echo '<b>'.$name.'</b> '.$emailId.'<br>';
 echo $emailId.'<br>';
 echo $number.'<br>';
